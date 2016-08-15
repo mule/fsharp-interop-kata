7f65b1aec7db0412cd494a5e27c1900feea9b87f
Jukka Puranen <jukka.puranen@gmail.com>
2016-08-15 10:40:35 +0300

    Changed the whole thing to capture only foreground window. It seems that it is not very practical to capture every window unless you force each one into foreground in turn.

--- Program.fs
+++ Program.fs
@@ -1,9 +1,11 @@
 ﻿
 
+
 module Win32 =
     open System
     open System.Text
     open System.Drawing 
+    open System.Drawing.Imaging
     open System.Runtime.InteropServices
     type EnumWindowsProc = delegate of IntPtr * IntPtr -> bool
 
@@ -25,11 +27,18 @@ module Win32 =
     [<DllImport("user32.dll")>]
     extern bool EnumWindows(EnumWindowsProc callback, IntPtr lParam)
 
+    [<DllImport("user32.dll")>]
+    extern bool PrintWindow(IntPtr hwnd, IntPtr hdc, UInt32 nFlags)
+
+    
+    [<DllImport("user32.dll")>]
+    extern IntPtr GetForegroundWindow()
+
 
     let getWindowTitle windowHandle = 
         let buffer = new StringBuilder(100)
         let result = GetWindowText(windowHandle, buffer, 100 )
-        buffer
+        buffer.ToString()
 
     let getWindowBounds windowHandle =
         let mutable rect : RECT = Unchecked.defaultof<_>
@@ -45,18 +54,38 @@ module Win32 =
         let enumResult = EnumWindows(callback, IntPtr.Zero)
         windows
 
-    let windowToString (title: string,  bounds: Rectangle) : string =
+    let windowDataToString (title: string,  bounds: Rectangle) : string =
         let windowStr = sprintf " tittle: %s width: %d height: %d X: %d Y: %d" title bounds.Width bounds.Height bounds.X bounds.Y
         windowStr
 
+    let capture( windowHandle: IntPtr, image: Bitmap) : Bitmap =         
+        use g = Graphics.FromImage(image)
+        let dc = g.GetHdc()
+        let result = PrintWindow(windowHandle, dc, UInt32.MinValue) 
+        g.ReleaseHdc(dc)
+        image
 
-            
+    let imageSave(fileNameWithPath : string, image : Image) =
+        image.Save(fileNameWithPath, ImageFormat.Png)
+
+    let isValidWindowForCapturing( bounds : Rectangle) : bool = 
+        bounds.Height > 0 && bounds.Width > 0
+
+
+        
+        
 module Test = 
     printfn "%A" (Win32.enumTopWindows())
 
 
 [<EntryPoint>]
 let main argv =
-    let windowDataList = Win32.enumTopWindows() |> List.map( fun windowHandle -> ( Win32.getWindowTitle(windowHandle).ToString(), Win32.getWindowBounds(windowHandle) ))
-    windowDataList |> List.iter( fun windowData ->(Win32.windowToString( fst windowData, snd windowData) |> printf "%s\n" ))
+    let foregroundWindowHandle = Win32.GetForegroundWindow()
+    let windowTitle = Win32.getWindowTitle(foregroundWindowHandle)
+    let windowBounds = Win32.getWindowBounds(foregroundWindowHandle)
+    let filename = System.Guid.NewGuid().ToString() |> sprintf "C:\\Temp\\%s.png" 
+    printf "\nCapturing %s to file %s" windowTitle filename
+    use mutable image = new System.Drawing.Bitmap(windowBounds.Width, windowBounds.Height)
+    let image = Win32.capture(foregroundWindowHandle, image)
+    Win32.imageSave(filename, image)
     0 // return an integer exit code
